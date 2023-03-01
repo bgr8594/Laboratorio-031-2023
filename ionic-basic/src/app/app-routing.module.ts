cf0d529fd8c6a6428c8b3c7ec18b3070aa13f4d0
Benito <bgr8594@gmail.com>
2023-02-28 20:36:14 -0600

    Practica 05
    Navegación con Tabs, Menu flotante como componente global

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
     path: 'detalle-receta',
     loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
 ];
 
 @NgModule({
--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FloatMenuComponent } from './componentes/float-menu.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FloatMenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
