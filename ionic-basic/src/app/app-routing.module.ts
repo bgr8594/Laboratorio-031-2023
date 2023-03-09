6122c0a5eb75cebe872b52b3c045ba668edd7a13
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2023-03-09 00:23:01 -0600

    Practica 3
    Practica 3 de Laboratorio de Aplicaciones Moviles

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -15,6 +15,18 @@ const routes: Routes = [
     path: 'presupuesto',
     loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
   },
+  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'receptor',
+    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
+  },
 ];
 
 @NgModule({
