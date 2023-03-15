f75bad48044c9a850f0ec4b5e4efe8429a8e80da
Benito <bgr8594@gmail.com>
2023-03-14 20:40:01 -0600

    Practica 07
    Validacion de formularios, estilos globales scss

--- ionic-basic/src/app/register/register.page.ts
+++ ionic-basic/src/app/register/register.page.ts
@@ -3,6 +3,9 @@ import { AutService } from '../service/aut.service';
 import { Router } from '@angular/router';
 import { User } from '../interface/user';
 import { MenuServiceService } from '../service/menu-service.service';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { ModalErrorComponent } from '../componentes/modal-error.component';
 
 @Component({
   selector: 'app-register',
@@ -12,16 +15,34 @@ import { MenuServiceService } from '../service/menu-service.service';
 export class RegisterPage implements OnInit {
 
   user: User = new User();
+  formRegister : any;
 
   constructor(
     private autSvc: AutService,
     private router: Router,
-    private menuService: MenuServiceService
-  ) { }
+    private menuService: MenuServiceService,
+    private formBuilder: FormBuilder,
+    private modalCtrl: ModalController
+  ) { 
+    this.buildForm();
+    this.formRegister.reset();
+  }
 
   ngOnInit() {
   }
 
+  submitForm(){
+    if(this.formRegister.valid){
+      this.user.email = this.formRegister.get('email').value;
+      this.user.password = this.formRegister.get('password').value;
+      this.onRegister();
+    }
+  }
+
+  ionViewWillEnter(){
+    this.formRegister.reset();
+  }
+  
   async onRegister(){
     this.autSvc.onRegister(this.user).then(user=>{
       if(user){
@@ -29,7 +50,10 @@ export class RegisterPage implements OnInit {
         this.router.navigate(['/login']);
       }
     }).catch(error=>{
-      console.log('Error al crear usuario!');
+      if(error.code =='auth/email-already-in-use'){
+        this.openModal(error);
+      }
+      console.log(error.code);
     })
 
   }
@@ -38,4 +62,26 @@ export class RegisterPage implements OnInit {
     this.router.navigate(["/login"]);
   }
 
+  hasError: any = (controlName: string, errorName: string) => {
+		return !this.formRegister.controls[controlName].valid &&
+			this.formRegister.controls[controlName].hasError(errorName) &&
+			this.formRegister.controls[controlName].touched;
+	}
+
+  buildForm(){
+    this.formRegister = this.formBuilder.group({
+      email: new FormControl('',{validators: [Validators.email,Validators.required]}),
+      password: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(6)]})
+    });
+  }
+
+  async openModal(user: any){
+    const modal = await this.modalCtrl.create({
+      component: ModalErrorComponent,
+      componentProps:{
+        error: 'Error al crear el usuario'
+      }
+    });
+    return await modal.present();
+  }
 }
