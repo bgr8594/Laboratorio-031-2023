10b1ae578cca55ecac374fb0673ae971d55b8fae
Mario <mario13bernal@gmail.com>
2023-04-13 13:34:05 -0600

    Practica 8
    Alta lugar en firestore, consulta de lugar una sola ves, evento de volver a pagina destinos.

---
+++ ionic-basic/src/app/interface/lugar.ts
@@ -0,0 +1,13 @@
+export class Lugar {
+    nombre: string;
+    ubicacion?: any = {latitud:'', longitud:''};
+
+    constructor(){
+        this.nombre = ''
+    }
+
+    public setUbicacion(latitud: string, Longiutd: string){
+        this.ubicacion.latitud= latitud;
+        this.ubicacion.longitud = Longiutd;
+    }
+}
--- ionic-basic/src/app/service/auth.service.ts
+++ ionic-basic/src/app/service/auth.service.ts
@@ -5,9 +5,13 @@ import { initializeApp } from "firebase/app";
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Lugar } from '../interface/lugar';
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 
 const firebaseApp = initializeApp(environment.firebaseConfig);
+const dbCloudFirestore = getFirestore(firebaseApp);
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +20,7 @@ export class AuthService {
 
   public isLoged : any = false;
   auth: Auth;
+  db = dbCloudFirestore;
   constructor() {
     this.auth = getAuth(firebaseApp);
     onAuthStateChanged(this.auth, user => {
@@ -37,4 +42,30 @@ export class AuthService {
 onRegister(user: User): Promise<any>{
   return  createUserWithEmailAndPassword(this.auth, user.email, user.password);
 }
+
+async altaLugar(lugar: Lugar){
+  const lugarTemp: any ={
+    nombre:lugar.nombre,
+    ubicacion: {longitud:'', latitud:''}
+  };
+  const docRef = await addDoc(collection(this.db,'lugar'), lugarTemp);
+  console.log("Documento escrito con id: "+docRef.id);
+}
+
+async getLugares(destinos: Lugar[]) {
+  await getDocs(collection(this.db, 'lugar')).then((querySnapshot: any)=>{
+    destinos.splice(0, destinos.length);
+    querySnapshot.forEach((doc: any)=>{
+      let data: any = doc.data();
+        let lugar: Lugar = new Lugar();
+        lugar.nombre = data.nombre;
+        console.log(doc.id);
+        destinos.push(lugar);
+    });
+  })
+  .catch(error=>{
+    console.log('Ocurrio un erro en el guardardo:'+error);
+  });
+} 
+
 }
