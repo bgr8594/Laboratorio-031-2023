767e2a6a67516160228727bee6fe9b632a6e43b2
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2023-04-14 01:31:27 -0600

    Practica 7
    Practica 7 de Laboratorio de Aplicaciones Moviles

--- ionic-basic/src/app/login/login.page.ts
+++ ionic-basic/src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import { ModalController } from '@ionic/angular';
 import { AutService} from '../service/aut.service';
 import { Router } from '@angular/router';
 import { MenuServiceService } from '../service/menu-service.service';
+import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,16 +15,19 @@ import { MenuServiceService } from '../service/menu-service.service';
 export class LoginPage implements OnInit {
 
   user: User = new User();
+  ionicForm: any;
 
   constructor(
     private router: Router,
     private modalCtrl: ModalController,
     private autSvc: AutService,
-    private menuService: MenuServiceService
+    private menuService: MenuServiceService,
+    private formBuilder: FormBuilder
   ) {
   }
 
   ngOnInit() {
+    this.buildForm();
   }
 
   async onLogin(){
@@ -60,5 +64,37 @@ export class LoginPage implements OnInit {
     onRegister(){
       this.menuService.setTitle("register")
       this.router.navigate(['/register']);
+    }
+
+    buildForm(){
+      this.ionicForm = this.formBuilder.group({
+        email: new FormControl('',{validators: [Validators.email,Validators.required]}),
+        password: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(6)]})
+      });
+    }  
+    
+    hasError: any = (controlName: string, errorName: string) => {
+      return !this.ionicForm.controls[controlName].valid &&
+        this.ionicForm.controls[controlName].hasError(errorName) &&
+        this.ionicForm.controls[controlName].touched;
+    }   
+    
+    submitForm(){
+      if(this.ionicForm.valid){
+        this.user.email = this.ionicForm.get('email').value;
+        this.user.password = this.ionicForm.get('password').value;
+        this.onLogin();
+      }
+    }
+    
+    notZero(control: AbstractControl) {
+      if (control.value && control.value.monto <= 0) {
+        return { 'notZero': true };
+      }
+      return null;
+    } 
+  
+    ionViewWillEnter(){
+      this.ionicForm.reset();
     }
 }
