713c70bd39c1a4afa62867be4c19b4d4a9dffa83
joeleduardo19 <joelvargasrivera5@gmail.com>
2023-04-14 18:34:16 -0500

    Practica 8

--- ionic-basic/src/app/componentes/float-menu.component.ts
+++ ionic-basic/src/app/componentes/float-menu.component.ts
@@ -12,74 +12,96 @@ import { onAuthStateChanged } from 'firebase/auth';
 })
 export class FloatMenuComponent implements OnInit, OnDestroy {
 
-  datosMenu: Menu[] =[
-    {nombre: 'login',enlace:'/login',
-   icono:'log-in-outline'},
-   {nombre: 'logout',enlace:'/home',
-   icono:'log-out-outline'}
+  datosMenu: Menu[] = [
+    {
+      nombre: 'login', enlace: '/login',
+      icono: 'log-in-outline'
+    },
+    {
+      nombre: 'logout', enlace: '/home',
+      icono: 'log-out-outline'
+    }
   ];
 
-  titleMenu: string='home';
+  titleMenu: string = 'home';
 
-   public isLoged : any = false;
+  public isLoged: any = false;
 
-   public subscription : Subscription = new Subscription();
+  public subscription: Subscription = new Subscription();
 
   constructor(
     private autService: AutService,
     private menuService: MenuServiceService,
     private router: Router
   ) {
-    onAuthStateChanged(this.autService.getStateAuth(), user=>{
-          if(user!=null && user != undefined){
-            this.isLoged = true;
-          }
-        });
-        this.subscription = this.menuService.$getTitleMenu.subscribe(
-          data=>{
-            console.log(data);
-            this.titleMenu =data;
-          }
-      );
+    onAuthStateChanged(this.autService.getStateAuth(), user => {
+      if (user != null && user != undefined) {
+        this.isLoged = true;
+      }
+    });
+    this.subscription = this.menuService.$getTitleMenu.subscribe(
+      data => {
+        console.log(data);
+        this.titleMenu = data;
+      }
+    );
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  navegar(link: string, titleMenu: string){
-     this.titleMenu =titleMenu;
-     this.router.navigate([link]);
-   }
+  navegar(link: string, titleMenu: string) {
+    this.titleMenu = titleMenu;
+    this.router.navigate([link]);
+  }
 
   ngOnDestroy(): void {
-    if(this.subscription != null || this.subscription!= undefined){
+    if (this.subscription != null || this.subscription != undefined) {
       this.subscription.unsubscribe();
     }
   }
-  onMenuOpen(){
-     onAuthStateChanged(this.autService.getStateAuth(), user=>{
-       if(user!=null && user != undefined){
-         this.datosMenu =[
-           {nombre: 'Alumnos',enlace:'/alumnos',
-     icono:'school-outline'},
-       {nombre: 'Receteas',enlace:'/receta',
-       icono:'restaurant-outline'},
-       {nombre: 'Presupuesto',enlace:'/presupuesto',
-       icono:'cash-outline'},
-       {nombre: 'inicio',enlace:'/inicio',
-       icono:'navigate-outline'},
-       {nombre: 'Tabs',enlace:'/tabs',
-       icono:'folder-outline'},
-           {nombre: 'logout',enlace:'/home',
-           icono:'log-out-outline'}
-         ];
+  onMenuOpen() {
+    onAuthStateChanged(this.autService.getStateAuth(), user => {
+      if (user != null && user != undefined) {
+        this.datosMenu = [
+          {
+            nombre: 'Alumnos', enlace: '/alumnos',
+            icono: 'school-outline'
+          },
+          {
+            nombre: 'Receteas', enlace: '/receta',
+            icono: 'restaurant-outline'
+          },
+          {
+            nombre: 'Presupuesto', enlace: '/presupuesto',
+            icono: 'cash-outline'
+          },
+          {
+            nombre: 'inicio', enlace: '/inicio',
+            icono: 'navigate-outline'
+          },
+          {
+            nombre: 'Turismo', enlace: '/destinos',
+            icono: 'airplane'
+          },
+          {
+            nombre: 'Tabs', enlace: '/tabs',
+            icono: 'folder-outline'
+          },
+          {
+            nombre: 'logout', enlace: '/home',
+            icono: 'log-out-outline'
+          }
+        ];
 
-       }
-      else{
-         this.datosMenu =[
-           {nombre: 'login',enlace:'/login',
-           icono:'log-in-outline'}
-         ];
-       }
-     });
-   }
+      }
+      else {
+        this.datosMenu = [
+          {
+            nombre: 'login', enlace: '/login',
+            icono: 'log-in-outline'
+          }
+        ];
+      }
+    });
+  }
 }
