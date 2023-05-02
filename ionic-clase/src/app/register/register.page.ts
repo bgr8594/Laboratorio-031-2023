c75eebcd8c4b7a9ee8cddeede73e02c9d929ff82
CarlosDavidMtz <99464049+CarlosDavidMtz@users.noreply.github.com>
2023-05-01 23:34:35 -0600

    Practica06

---
+++ ionic-clase/src/app/register/register.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AutService } from '../service/aut.service';
+import { Router } from '@angular/router';
+import { User } from '../interface/user';
+import { MenuServiceService } from '../service/menu-service.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  user: User = new User();
+
+  constructor(
+    private autSvc: AutService,
+    private router: Router,
+    private menuService: MenuServiceService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  async onRegister(){
+    this.autSvc.onRegister(this.user).then(user=>{
+      if(user){
+        console.log('Successfully created user!');
+        this.router.navigate(['/login']);
+      }
+    }).catch(error=>{
+      console.log('Error al crear usuario!');
+    })
+
+  }
+  onLogin(){
+    this.menuService.setTitle("login");
+    this.router.navigate(["/login"]);
+  }
+
+}
