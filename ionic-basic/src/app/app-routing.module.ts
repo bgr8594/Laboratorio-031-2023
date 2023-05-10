2e8204210ef8fb72a672411a087abb7732360b94
benito.gonzalez <bgr8594@gmail.com>
2023-05-09 21:00:07 -0600

    Practica 12
    Galeira, pwa elements, Capacitor/camera, Capacitor/Filesystem, Capacitor/Preference

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -12,47 +12,44 @@ const routes: Routes = [
     children: [
       {
         path: 'presupuesto',
-        loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
       },
       {
         path: 'alumnos',
-        loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
       },
       {
         path: 'inicio',
-        loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
       },
       {
         path: 'receptor',
-        loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
       },
       {
         path: 'receta',
-        loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
       },
       {
         path: 'detalle-receta',
-        loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
       },
       {
         path: 'tabs',
-        loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
       },
       {
         path: 'destinos',
-        loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule),
-        canActivate: [AutGuardGuard]
+        loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
       },
       {
         path: 'destinos-api',
         loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
+      }
+      ,
+      {
+        path: 'galeria',
+        loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
       }
     ],
     canActivate: [AutGuardGuard]
