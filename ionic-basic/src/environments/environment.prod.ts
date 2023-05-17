0fd0de92de03a4acdc823ec7650abb6a48e4df62
benito.gonzalez <bgr8594@gmail.com>
2023-05-16 20:23:56 -0600

    Practica 13
    Ajuste para crear agregar capa ios/android

--- ionic-basic/src/environments/environment.prod.ts
+++ ionic-basic/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCDjZqokWsLJAWjY1qxgP8BlwYCVjpK6OE",
+    authDomain: "prueba-lab-031.firebaseapp.com",
+    projectId: "prueba-lab-031",
+    storageBucket: "prueba-lab-031.appspot.com",
+    messagingSenderId: "598817790682",
+    appId: "1:598817790682:web:a5c7a691b2071564ef23e3"
+  },
+  apiKeyMaps: 'AIzaSyDa2ba8bnGz9I2nUNePAk6SxUwWO4neIBE'
 };
