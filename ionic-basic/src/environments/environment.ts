6606db17ded65a25b27b4c7c138772f0cbbb7366
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2023-05-21 19:03:21 -0600

    Practica 11
    Practica 11 de Laboratorio de Aplicaciones Moviles

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -11,7 +11,8 @@ export const environment = {
     storageBucket: "prueba-lab-031.appspot.com",
     messagingSenderId: "598817790682",
     appId: "1:598817790682:web:a5c7a691b2071564ef23e3"
-  }
+  },
+  apiKeyMaps: 'AIzaSyDa2ba8bnGz9I2nUNePAk6SxUwWO4neIBE'
 };
 
 /*
