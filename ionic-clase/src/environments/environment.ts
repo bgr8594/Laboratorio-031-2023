ca53605263e85996866e58505294e52adf861b8e
EdgarOrtiz1828 <124111298+EdgarOrtiz1828@users.noreply.github.com>
2023-05-26 23:24:41 -0600

    Practica 13

--- ionic-clase/src/environments/environment.ts
+++ ionic-clase/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
    firebaseConfig: {
     apiKey: "AIzaSyCWg0JnYH0K4BMAHy-pUwduypQE-gpOBQE",
     authDomain: "lab-031-eeoc.firebaseapp.com",
